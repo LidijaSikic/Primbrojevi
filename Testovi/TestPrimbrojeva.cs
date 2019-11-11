3b66032e877fb5d3522f28b0a4622e8d56569214
Lidija Šikić <ligalic@vsite.hr>
2019-11-11 20:24:50 +0100

    Implement Unit Tests

--- Testovi/TestPrimbrojeva.cs
+++ Testovi/TestPrimbrojeva.cs
@@ -7,9 +7,31 @@ namespace Vsite.Pood
     public class TestPrimbrojeva
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GenerirajPrimbrojeveVraćaPrazanNizZaArgument0()
         {
+            Assert.AreEqual(0, Primbrojevi.GenerirajPrimBrojeve(0).Length);
+        }
+        [TestMethod]
+        public void GenerirajPrimbrojeveVraćaPrazanNizZaArgument1()
+        {
+            Assert.AreEqual(0, Primbrojevi.GenerirajPrimBrojeve(1).Length);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimbrojeveVraćaNizkojiSadržiSamo2ZaArgument2()
+        {
+            Assert.AreEqual(1, Primbrojevi.GenerirajPrimBrojeve(2).Length);
+            Assert.AreEqual(2, Primbrojevi.GenerirajPrimBrojeve(2)[0]);
 
         }
+
+        [TestMethod]
+        public void GenerirajPrimbrojeveVraćaNizkojiSadrži25ProstihBrojevaZaArgument100()
+        {
+            Assert.AreEqual(25, Primbrojevi.GenerirajPrimBrojeve(100).Length);
+            Assert.AreEqual(2, Primbrojevi.GenerirajPrimBrojeve(100)[0]);
+            Assert.AreEqual(97, Primbrojevi.GenerirajPrimBrojeve(100)[24]);
+        }
+
     }
 }
