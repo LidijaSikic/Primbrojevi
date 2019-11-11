68cb6a4fd257f3f53718e779c5d1bdf0be772f5d
Lidija Šikić <ligalic@vsite.hr>
2019-11-11 20:50:18 +0100

    Replace f with eliminirani

--- Primbrojevi/Primbrojevi.cs
+++ Primbrojevi/Primbrojevi.cs
@@ -8,7 +8,7 @@ namespace Vsite.Pood
     public class Primbrojevi
     {
         static int s;// duljina niza
-        static bool[] f ; // niz s primbrojevima
+        static bool[] eliminirani ; // niz s primbrojevima
         public static int[] GenerirajPrimBrojeve(int max)
         {
             if (max < 2)
@@ -32,7 +32,7 @@ namespace Vsite.Pood
             int broj = 0;
             for (int i = 0; i < s; ++i)
             {
-                if (f[i])
+                if (eliminirani[i]==false)
                     ++broj;
             }
 
@@ -41,7 +41,7 @@ namespace Vsite.Pood
             // prebaci primbrojeve u rezultat
             for (int i = 0, j = 0; i < s; ++i)
             {
-                if (f[i])
+                if (eliminirani[i]== false)
                     primovi[j++] = i;
             }
 
@@ -52,10 +52,10 @@ namespace Vsite.Pood
         {
             for (int i = 2; i < Math.Sqrt(s) + 1; ++i)
             {
-                if (f[i]) // ako je i prekrižen, prekriži i višekratnike
+                if (eliminirani[i]==false) // ako je i prekrižen, prekriži i višekratnike
                 {
                     for (int j = 2 * i; j < s; j += i)
-                        f[j] = false; // višekratnik nije primbroj
+                        eliminirani[j] = true; // višekratnik nije primbroj
                 }
             }
         }
@@ -63,15 +63,16 @@ namespace Vsite.Pood
         private static void InicijalizirajSito(int max)
         {
             s = max + 1;
-            f = new bool[s]; // niz s primbrojevima
-
-
-            // inicijaliziramo sve na true
-            for (int i = 0; i < s; ++i)
-                f[i] = true;
+            eliminirani = new bool[s]; // niz s primbrojevima
 
             // ukloni 0 i 1 koji su primbrojevi po definiciji
-            f[0] = f[1] = false;
+            eliminirani[0] = eliminirani[1] = true;
+
+            // inicijaliziramo sve na true
+            for (int i = 2; i < s; ++i)
+                eliminirani[i] = false;
+
+            
         }
     }
 }
