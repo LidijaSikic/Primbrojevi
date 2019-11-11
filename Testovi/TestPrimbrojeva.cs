c6d9a075a9f516f2eb494bc31821cca0d5c5b3c5
Lidija Šikić <ligalic@vsite.hr>
2019-11-11 20:09:46 +0100

    Add Test project

---
+++ Testovi/TestPrimbrojeva.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Pood
+{
+    [TestClass]
+    public class TestPrimbrojeva
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
